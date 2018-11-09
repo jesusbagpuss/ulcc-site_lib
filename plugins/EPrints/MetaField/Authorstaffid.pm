383fbe0325c52b78ba7749020211612cef54b094
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2018-11-09 04:37:50 +0000

    allow use of authostaffid for any old sub in any old field

--- plugins/EPrints/MetaField/Authorstaffid.pm
+++ plugins/EPrints/MetaField/Authorstaffid.pm
@@ -18,30 +18,33 @@ BEGIN {
 
 sub _get_name {
     my ( $self, $session, $value ) = @_;
-
-    return "" unless ( EPrints::Utils::is_set($value) );
+    
+   return "" unless ( EPrints::Utils::is_set($value) );
+    
+   return $value if(!defined $self->{parent_name}); #Authorstafid only really any use as part of a compound
 
     my $name = $value;
-
     my $ds = $session->dataset("eprint");
     my $searchexp = $ds->prepare_search();
+    my $field = $ds->field($self->get_name);
     $searchexp->add_field(
-        fields => [ $ds->field('creators_staffid') ],
+        fields => [ $field ],
         value  => $value,
         match  => "EQ",
     );
 
     my $eprint = $searchexp->perform_search->item(0);
-
     if ( defined $eprint ) {
-        for my $creator ( @{ $eprint->get_value("creators") } ) {
-            if ( defined $creator->{staffid} ) {
-                if ( $creator->{staffid} eq $value ) {
-                    $name =
-                      EPrints::Utils::make_name_string( $creator->{name} );
-                }
-            }
-        }
+	my $parent_name = $self->{parent_name};
+	my $sub_name = $self->{sub_name};
+	for my $item ( @{ $eprint->get_value($parent_name) } ) {
+	    if ( defined $item->{$sub_name} ) {
+		if ( $item->{$sub_name} eq $value ) {
+		    #NB : this assumes that the aprent compound has a sub field called "name"
+		    $name = EPrints::Utils::make_name_string( $item->{name} );
+		}
+	    }
+	}
     }
     return $name;
 }
@@ -64,3 +67,5 @@ sub render_single_value {
 }
 
 1;
+
+
