af72031349e224bd7ad2963bea0fee5d7ebe8578
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2019-11-16 20:23:01 +0000

    turn on long_format_url for all to imrpove gscolar indexing

---
+++ cfg.d/20_baseurls.pl
@@ -0,0 +1,38 @@
+# These configuration variables are mostly unused and should be deprecated.
+
+{
+	my $uri = URI->new( "http://" );
+    #Introduce an https bias in forming urls if secure host is set at all
+	if( !EPrints::Utils::is_set( $c->{securehost} ) )
+	{
+		$uri->scheme( "http" );
+		$uri->host( $c->{host} );
+		$uri->port( $c->{port} );
+		$uri = $uri->canonical;
+		$uri->path( $c->{http_root} );
+	}
+	else
+	{
+		$uri->scheme( "https" );
+		$uri->host( $c->{securehost} );
+		$uri->port( $c->{secureport} );
+		$uri = $uri->canonical;
+		$uri->path( $c->{https_root} );
+	}
+
+# EPrints base URL without trailing slash
+	$c->{base_url} = "$uri";
+# CGI base URL without trailing slash
+	$c->{perl_url} = "$uri/cgi";
+}
+
+# If you don't want EPrints to respond to a specific URL add it to the
+# exceptions here. Each exception is matched against the uri using regexp:
+#  e.g. /myspecial/cgi
+# Will match http://yourrepo/myspecial/cgi
+#$c->{rewrite_exceptions} = [];
+#
+
+#if turned on, the abstract page url will be: http://domain.com/id/eprint/43/. This format helps google scholar to index eprints repository.
+#if turned off: http://domain.com/43/
+$c->{use_long_url_format} = 1;
